cfac1862a3e865390b23306e7ad5c4f9538988e5
Krzysztof Bociurko <krzysztof.bociurko@bivrost.pro>
2021-09-02 21:21:39 +0200

    Rollup and terser build system

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+(
+cat << END
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Death And Taxes JS13K</title>
+	</head>
+	<body style="margin:0">
+		<script src="https://js13kgames.com/webxr-src/2021/three.js"></script>
+		<script>
+END
+cat ZzFX.micro.js ZzFX-sounds.js | npx terser -c passes=2 -m
+cat << END
+        </script>
+		<script type="module">
+END
+
+npx rollup --format es init.js | npx terser --module -c passes=2 -m #--mangle-props
+
+cat << END
+		</script>
+	</body>
+</html>
+END
+) > minified.html
+
+F=$(mktemp -u).zip
+trap "rm \"$F\"" exit
+7z a -mm=Deflate -mfb=258 -mpass=15 "$F" minified.html >/dev/null
+wc "$F" -c
+
---
+++ init.js
@@ -0,0 +1,13 @@
+import XRButtonLite from "./XRButtonLite.js";
+import { camera, renderer } from "./game.js";
+XRButtonLite(document.body);
+
+if(THREE.OrbitControls) {
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.minDistance = 1;
+    controls.maxDistance = 5;
+    controls.target.set(0, 1.7);
+    controls.update();
+}
+camera.position.set(0, 10, 0);
+camera.lookAt(0,0,0);
