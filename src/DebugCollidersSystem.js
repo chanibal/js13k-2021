7b4286fe776815c79b872dbcf212120d7bc2a64c
Krzysztof Bociurko <krzysztof.bociurko@bivrost.pro>
2021-09-10 01:42:58 +0200

    Collider debug widget for capsules

--- src/DebugCollidersSystem.js
+++ src/DebugCollidersSystem.js
@@ -6,7 +6,11 @@ export class DebugCollidersSystem {
         this.selector = ecs.select(Transform);
         this.helpers = [];
         this.mat = new THREE.MeshBasicMaterial({ wireframe: true, color: 0xff00ff });
+        this.mat2 = new THREE.MeshBasicMaterial({ wireframe: true, color: 0xffff00 });
         this.sphere = new THREE.SphereGeometry(1, 8, 4);
+        this.sphereTop = new THREE.SphereGeometry(1, 8, 2, 0, Math.PI * 2, 0, Math.PI/2);
+        this.sphereBottom = new THREE.SphereGeometry(1, 8, 2, 0, Math.PI * 2, Math.PI/2, Math.PI/3);
+        this.cylinder = new THREE.CylinderGeometry(1,1,1,8,1,true);
     }
     update(dt) {
         // FIXME: Port to InstancedMesh?
@@ -25,9 +29,28 @@ export class DebugCollidersSystem {
                 return;
 
             let helper;
-            if (t.collider.r) {
+            let h = (t.collider.h || 0) / 2;
+            let r = (t.collider.r || 0);
+            if (h) {
+                helper = new THREE.Group();
+                const sphereTop = new THREE.Mesh(this.sphereTop, this.mat);
+                sphereTop.scale.set(r,r,r);
+                sphereTop.position.y += h;
+
+                const sphereBottom = new THREE.Mesh(this.sphereBottom, this.mat);
+                sphereBottom.scale.set(r,r,r);
+                sphereBottom.position.y -= h;
+
+                const cylinder = new THREE.Mesh(this.cylinder, this.mat);
+                cylinder.scale.set(r,h*2,r)
+
+                helper.add(sphereTop);
+                helper.add(sphereBottom);
+                helper.add(cylinder);
+            }
+            else if (r) {
                 helper = new THREE.Mesh(this.sphere, this.mat);
-                helper.scale.set(t.collider.r, t.collider.r, t.collider.r);
+                helper.scale.set(r,r,r);
             }
             else {
                 helper = new THREE.AxesHelper(0.1);
