31ea53e2efd527da63ad4eaeb4c980e3e74e256e
Krzysztof Bociurko <krzysztof.bociurko@bivrost.pro>
2021-09-10 00:18:16 +0200

    Nicer debug info

--- src/init.js
+++ src/init.js
@@ -1,6 +1,9 @@
 import XRButtonLite from "./XRButtonLite.js";
-import { camera, renderer } from "./game.js";
 import { zzfx_volume } from "./ZzFX.micro.js";
+zzfx_volume(0.0);
+
+
+import { camera, renderer } from "./game.js";
 import * as G from "./game.js";
 window.G = G;
 
@@ -9,17 +12,16 @@ try {
 } catch (ex) {
     const popup = document.createElement("div");
     const s = popup.style;
-        s.position="absolute";
-        s.bottom = s.left = s.right = "5%";
-        s.border = "3px double red";
-        s.padding = "1em";
-        s.color = "red";
-        s.backgroundColor = "#000000cc";
+    s.position="absolute";
+    s.bottom = s.left = s.right = "5%";
+    s.border = "3px double red";
+    s.padding = "1em";
+    s.color = "red";
+    s.backgroundColor = "#000000cc";
     popup.textContent = `ERROR:\n${ex}`;
     document.body.appendChild(popup);
 }
 
-zzfx_volume(0.1);
 
 if(THREE.OrbitControls) {
     const controls = new THREE.OrbitControls(camera, renderer.domElement);
@@ -36,13 +38,14 @@ camera.lookAt(0,0,0);
 if(true) {
     const popup = document.createElement("pre");
     const s = popup.style;
-        s.position="absolute";
-        s.top = s.left = "5%";
-        s.border = "3px double red";
-        s.padding = "1em";
-        s.color = "green";
-        s.backgroundColor = "#000000cc";
+    s.position="absolute";
+    s.top = s.left = "5%";
+    s.padding = "1em";
+    s.color = "green";
     document.body.appendChild(popup);
-    const update = () => { popup.textContent = [renderer.info.memory, renderer.info.render, ecs_stats].map(o => JSON.stringify(o, undefined, "  " )).join("\n"); requestAnimationFrame(update); };
-    update();
+    let ecs_stats_local = {};
+    setInterval(() => { 
+        for(let s of Object.keys(window.ecs_stats)) ecs_stats_local[s] = Math.round(ecs_stats[s] * 1000);
+        popup.textContent = [renderer.info.memory, renderer.info.render, ecs_stats_local].map(o => JSON.stringify(o, undefined, "  " )).join("\n");
+    }, 100);
 }
