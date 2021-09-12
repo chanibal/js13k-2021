99adf201937c8521a995ae9e1c1e2c603e3ce695
Krzysztof Bociurko <krzysztof.bociurko@bivrost.pro>
2021-09-12 06:24:00 +0200

    Modified build system, added intro

--- build.sh
+++ build.sh
@@ -1,29 +1,16 @@
 #!/usr/bin/env bash
 
 (
-cat << END
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Death And Taxes JS13K</title>
-	</head>
-	<body style="margin:0">
-		<script src="https://js13kgames.com/webxr-src/2021/three.js"></script>
-		<script type="module">
-END
+	head game.html -n 7
+	echo ' 		<script src="https://js13kgames.com/webxr-src/2021/three.js"></script>'
+	echo '      <script type="module">'
+	npx rollup --format es src/init.js | npx terser --module -c passes=2 -m  #--mangle-props
+	echo ' 		</script>'
+	tail game.html -n +10
+) >index.html
 
-npx rollup --format es src/init.js | npx terser --module -c passes=2 -m #--mangle-props
-
-cat << END
-		</script>
-	</body>
-</html>
-END
-) > minified.html
-
-F=$(mktemp -u).zip
-trap "rm \"$F\"" exit
-7z a -mm=Deflate -mfb=258 -mpass=15 "$F" minified.html >/dev/null
+F="js13k-2021-chanibal.zip"
+#trap "rm \"$F\"" exit
+7z a "$F" -mx 9 index.html >/dev/null
 wc "$F" -c
 
